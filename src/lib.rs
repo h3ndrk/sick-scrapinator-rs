2659224fb51e70bea388ae0f79c677705c298f0b
Hendrik <git@h3ndrk.de>
2022-10-05 22:18:03 +0200

    Refactor as library

--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,52 @@
 use std::{
     io::{Read, Write},
-    net::TcpStream,
+    net::{TcpStream, ToSocketAddrs},
     str::from_utf8,
     thread::sleep,
-    time::{Duration, Instant},
+    time::Duration,
 };
 
 use nom::{
     branch::alt,
     bytes::streaming::{tag_no_case, take_till1, take_while1, take_while_m_n},
-    character::{
-        complete::space1, is_alphanumeric, is_digit, is_hex_digit, is_space, streaming::hex_digit1,
-    },
-    combinator::{map, value},
+    character::{complete::space1, is_digit, is_space, streaming::hex_digit1},
+    combinator::map,
     multi::{count, length_count},
     sequence::{terminated, tuple},
     IResult,
 };
-use pretty_hex::PrettyHex;
 
-fn main() {
-    // println!("{}", line!());
-    // let socket = UdpSocket::bind("192.168.0.42:2213").unwrap();
-    // println!("{}", line!());
-    // socket.connect("192.168.0.2:2213").unwrap();
-    // println!("{}", line!());
-    // let telegram = vec![
-    //     0x02, 0x02, 0x02, 0x02, 0x00, 0x00, 0x00, 0x17, 0x73, 0x4d, 0x4e, 0x20, 0x53, 0x65, 0x74,
-    //     0x41, 0x63, 0x63, 0x65, 0x73, 0x73, 0x4d, 0x6f, 0x64, 0x65, 0x20, 0x03, 0xf4, 0x72, 0x47,
-    //     0x44, 0xb3,
-    // ];
-    // socket.send(&telegram).unwrap();
-    // println!("{}", line!());
-    // let mut response = [0; 4096];
-    // socket.recv(&mut response).unwrap();
-    // println!("{}", line!());
-    let mut socket = TcpStream::connect("192.168.0.2:2112").unwrap();
-    println!("connected");
-    let mut unparsed: Vec<u8> = vec![];
-    // println!("waiting for ready...");
-    // wait_for_ready(&mut socket, &mut unparsed);
-    println!("setting access mode...");
-    set_access_mode(&mut socket, &mut unparsed);
-    println!("starting measurement...");
-    start_measurement(&mut socket, &mut unparsed);
-    // println!("waiting for ready...");
-    // wait_for_ready(&mut socket, &mut unparsed);
-    println!("setting active applications...");
-    set_active_applications(
-        &mut socket,
-        &mut unparsed,
-        Application::Field,
-        ApplicationActivation::Disabled,
-    );
-    set_active_applications(
-        &mut socket,
-        &mut unparsed,
-        Application::Ranging,
-        ApplicationActivation::Enabled,
-    );
-    println!("runnnnnning...");
-    run(&mut socket, &mut unparsed);
-    println!("waiting for ready...");
-    wait_for_ready(&mut socket, &mut unparsed);
-    println!("done.");
-    let begin = Instant::now();
-    let mut number_of_scan_datas = 0;
-    while begin.elapsed() < Duration::from_secs(1) {
-        get_scan_data(&mut socket, &mut unparsed);
-        number_of_scan_datas += 1;
+pub struct Lidar {
+    socket: TcpStream,
+    unparsed: Vec<u8>,
+}
+
+impl Lidar {
+    pub fn connect(address: impl ToSocketAddrs) -> Self {
+        let mut socket = TcpStream::connect(address).unwrap();
+        let mut unparsed = vec![];
+        set_access_mode(&mut socket, &mut unparsed);
+        start_measurement(&mut socket, &mut unparsed);
+        set_active_applications(
+            &mut socket,
+            &mut unparsed,
+            Application::Field,
+            ApplicationActivation::Disabled,
+        );
+        set_active_applications(
+            &mut socket,
+            &mut unparsed,
+            Application::Ranging,
+            ApplicationActivation::Enabled,
+        );
+        run(&mut socket, &mut unparsed);
+        wait_for_ready(&mut socket, &mut unparsed);
+        Self { socket, unparsed }
     }
-    dbg!(number_of_scan_datas);
-    // let telegram = vec![
-    //     0x02, 0x73, 0x4D, 0x4E, 0x20, 0x53, 0x65, 0x74, 0x41, 0x63, 0x63, 0x65, 0x73, 0x73, 0x4D,
-    //     0x6F, 0x64, 0x65, 0x20, 0x30, 0x33, 0x20, 0x46, 0x34, 0x37, 0x32, 0x34, 0x37, 0x34, 0x34,
-    //     0x03,
-    // ];
-    // socket.write_all(b"\x02sMN SetAccessMode ").unwrap();
-    // socket.write_all(b"\x02sRN SCdevicestate\x03").unwrap();
-    // println!("written");
-    // let mut response = [0; 4096];
-    // let bytes = socket.read(&mut response).unwrap();
-    // let telegram = &response[..bytes];
-    // println!("")
+
+    pub fn poll_data(&mut self) -> Vec<usize> {
+        get_scan_data(&mut self.socket, &mut self.unparsed)
+    }
 }
 
 fn wait_for_ready(socket: &mut TcpStream, unparsed: &mut Vec<u8>) {
@@ -166,9 +130,7 @@ fn get_scan_data(socket: &mut TcpStream, unparsed: &mut Vec<u8>) -> Vec<usize> {
     Request::ScanData.write_to(socket);
     let response = get_next_response(socket, unparsed);
     match response {
-        Response::ScanData { values } => {
-            values
-        }
+        Response::ScanData { values } => values,
         _ => panic!("wut?!"),
     }
 }
@@ -268,8 +230,7 @@ fn response(input: &[u8]) -> IResult<&[u8], Response> {
             tuple((
                 tag_no_case("\x02sRA LMDscandata "),
                 count(tuple((take_till1(is_space), space1)), 17),
-                take_while_m_n(1, 1, is_digit),
-                space1,
+                tag_no_case("1 "),
                 count(tuple((take_till1(is_space), space1)), 5),
                 length_count(
                     map(terminated(hex_digit1, space1), |amount_of_data| {
@@ -279,20 +240,10 @@ fn response(input: &[u8]) -> IResult<&[u8], Response> {
                         usize::from_str_radix(from_utf8(value).unwrap(), 16).unwrap()
                     }),
                 ),
-                // alt((
-                //     value(ScalingFactor::FactorTimes1, tag_no_case("3F800000")),
-                //     value(ScalingFactor::FactorTimes2, tag_no_case("40000000")),
-                // )),
-                // tag_no_case(" 0 "),
                 take_while1(is_not_end),
                 tag_no_case("\x03"),
             )),
-            |(_, _, _amount_of_channels, _, _, values, _, _)| {
-                // let amount_of_channels =
-                //     u8::from_str_radix(from_utf8(amount_of_channels).unwrap(), 16).unwrap();
-                // dbg!(amount_of_channels);
-                Response::ScanData { values }
-            },
+            |(_, _, _, _, values, _, _)| Response::ScanData { values },
         ),
     ))(input)
 }
@@ -396,10 +347,4 @@ enum StopMeasurementResult {
 enum RunResult {
     Error,
     Success,
-}
-
-#[derive(Clone, Debug)]
-enum ScalingFactor {
-    FactorTimes1,
-    FactorTimes2,
 }
